74e9790dd6fe2ff6f8bd33c6173e0ed0f8eddad6
KenTandrian <kenricktan11@gmail.com>
2021-02-16 01:03:58 +0700

    Initial commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+/* scroll back button */
+scrollBtn = document.getElementById("scroll");
+
+window.onscroll = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
